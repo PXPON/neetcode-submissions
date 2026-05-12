261d5688f7fe489286aa0477375803859c4b2c35
Paxton Perry <paxton.perry@protonmail.com>
2026-05-12 00:29:09 -0700

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        forecast = [0] * len(temperatures)
+
+        # Put each value on stack one at a time and compare
+        # for i in range(1, len(temperatures) + 1):
+        for i in range(len(temperatures)):
+            # Check to see if temp is >= what's at the top of the stack
+
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                # Add currentIndex - previousIndex to previousIndex
+                val = stack.pop()
+                forecast[val] = i - val
+            stack.append(i)
+        
+        return forecast
+            
